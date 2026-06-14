512913e2150cc5ea3a503a7ccca08fcf1dd7ea0a
rana-621 <ranaebrahim621@gmail.com>
2026-06-14 14:39:03 +0300

    55 - Child Shop Component and what is ng Container in Angular

---
+++ src/app/shop/shop-item/shop-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shop-item',
+  standalone: false,
+  templateUrl: './shop-item.component.html',
+  styleUrl: './shop-item.component.scss'
+})
+export class ShopItemComponent {
+
+}
